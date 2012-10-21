68f9b88bbf8ce197d13cab0cd29cfbd40bc5eb36
Oximore <blux@blux-engine.(none)>
2012-10-21 16:56:00 +0200

    Ajout du Makefile systèmes_répartis/rmi/Chat/src

--- systemes_repartis/rmi/Chat/src/ServantChat.java
+++ systemes_repartis/rmi/Chat/src/ServantChat.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
 public class ServantChat implements Chat {
     
     public ServantChat(){
-	this.map = new HashMap<String,ChatBack>();
+	this._map = new HashMap<String,ChatBack>();
 
     }
 
-    private HashMap<String,ChatBack> map;
-
+    private HashMap<String,ChatBack> _map;
 
     // Begin Interface Chat
     public void connect(String nick, ChatBack ref) throws RemoteException {
-	if (map.get(nick) == null){
-	    map.put(nick,ref);
+	if (_map.get(nick) == null){
+	    _map.put(nick,ref);
 	} 
 	else {
 	    // TODO ERREUR
@@ -24,21 +23,41 @@ public class ServantChat implements Chat {
     }
     
     public void disconnect(String nick) throws RemoteException {
+	if ( _map.get(nick) != null){
+	    _map.remove(nick);
+	} 
+	else {
+	    // TODO ERREUR
+	}
     } 
     
     public ArrayList<String> list() throws RemoteException {
-	return null;
+	ArrayList<String> result = new ArrayList<String>();
+	for (String user : _map.keySet()){
+	    result.add(user);
+	}	
+	return result;
     } 
     
     public void send(String src, String msg) throws RemoteException {
+	ChatBack chatUser;
+	for (String user : _map.keySet()){
+	    if (!user.equals(src)) {
+		chatUser = _map.get(user);
+		try {
+		chatUser.send(src, msg);
+		} catch (RemoteException e) {
+		    // TODO ERREUR
+		}
+	    }
+	}	
     } 
     
     public void send(String src, String dst, String msg) throws RemoteException {
-	ChatBack ref = map.get(dst);
+	ChatBack ref = _map.get(dst);
 	if (ref != null) {
 	    try {
 		ref.send(src,msg);
-		// System.out.println
 	    } catch (RemoteException e) {
 		// TODO ERREUR
 	    }
